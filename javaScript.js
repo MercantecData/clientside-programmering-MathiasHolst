4521c6df51b3c35c8aa37c2ff961a371f89aad31
Mathias Fruergaard Holst <MFHo.skp>
2020-09-01 14:24:51 +0200

    +Grader i Viborg bliver vist som alert

--- javaScript.js
+++ javaScript.js
@@ -4,3 +4,10 @@ document.write(str.fontcolor("whitesmoke"));
 function closeTab(){
     window.close();
 }
+
+var url = "https://api.openweathermap.org/data/2.5/weather?q=Viborg&appid=41bfac970ccb62f946ceb789cef8bb08&units=metric";
+
+var promise = fetch(url);
+var promise2 = promise.then(v=>v.json());
+promise2.then(v=>console.log(v.main.temp))
+promise2.then(v=>alert(v.main.temp))
