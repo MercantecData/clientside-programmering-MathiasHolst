5e3fbde98ca78ffd8647e3a8e900454b90a70290
Mathias Fruergaard Holst <MFHo.skp>
2020-09-02 13:51:32 +0200

    +Jquery +Input til url'en -Progress med error

--- javaScript.js
+++ javaScript.js
@@ -1,13 +1,25 @@
+function start(){
+$("p").text("Hello world!");
 var str = new String("Hello World!");
-document.write(str.fontcolor("whitesmoke"));
-
-function closeTab(){
-    window.close();
+//document.write(str.fontcolor("whitesmoke"))
+//var url = "https://api.openweathermap.org/data/2.5/weather?lat=56.452027&lon=9.396347&appid=41bfac970ccb62f946ceb789cef8bb08&units=metric";
+var url = "https://api.openweathermap.org/data/2.5/weather?q=Viborg&appid=41bfac970ccb62f946ceb789cef8bb08&units=metric"
+var promise = fetch(url)
+var promise2 = promise.then(v=>v.json())
+promise2.then(v=>console.log(v.main.temp))
 }
 
-var url = "https://api.openweathermap.org/data/2.5/weather?q=Viborg&appid=41bfac970ccb62f946ceb789cef8bb08&units=metric";
+function closeTab(){
+    window.close()
+}
 
-var promise = fetch(url);
-var promise2 = promise.then(v=>v.json());
-promise2.then(v=>console.log(v.main.temp))
-promise2.then(v=>alert(v.main.temp))
+function getTempFromCity(){
+    var str = document.getElementById("text").value
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${str}&appid=41bfac970ccb62f946ceb789cef8bb08&units=metric` 
+    //document.getElementById("demo").innerHTML = "Denne by kunne ikke findes"
+    $("#demo").text("Denne by kunne ikke findes")
+    var promise = fetch(url)
+    var promise2 = promise.then(v=>v.json())
+    promise2.then(v=>document.getElementById("demo").innerHTML = (`${v.main.temp} Grader i ${v.name} men det foeles som ${v.main.feels_like}`))
+    
+}
