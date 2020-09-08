7036be8489912bb3f2221b5256f61643a966f1f6
Mathias Fruergaard Holst <MFHo.skp>
2020-09-08 13:38:01 +0200

    +Byer dropdown

---
+++ dropdownProject/javascript.js
@@ -0,0 +1,18 @@
+function callDubaiCity(){
+    document.getElementById("overskrift").innerHTML = ("Dubai")
+    document.getElementById("beskrivelse").innerHTML = ("Dubai er den største byggeplads i verden")
+    document.getElementById("billede1").src="https://www.costacruises.dk/content/dam/costa/inventory-assets/ports/DXB/dubai.jpg" 
+
+}
+
+function callTorontoCity(){
+    document.getElementById("overskrift").innerHTML = ("Toronto")
+    document.getElementById("beskrivelse").innerHTML = ("Toronto er den 5. største by i nord amerika")
+    document.getElementById("billede1").src="https://www.seetorontonow.com/wp-content/uploads/2020/06/Toronto-Skyline-view-from-lake.jpg" 
+}
+
+function callNewYorkCity(){
+    document.getElementById("overskrift").innerHTML = ("New York")
+    document.getElementById("beskrivelse").innerHTML = ("I New York bliver der snakket 800 forskellige sprog hvilket også gør det til den by med flest forskellige snakkede sprog i verden")
+    document.getElementById("billede1").src="https://cdn.getyourguide.com/img/location/5c04f6f20650d.jpeg/148.jpg"
+}
